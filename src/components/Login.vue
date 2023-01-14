db76fa6f086fb91351f23bf50f4971acb7c4b77e
yosshipaopao <yosshipaopao@gmail.com>
2023-01-15 02:37:12 +0900

    display data

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,16 @@
 <script setup>
 import { ref } from 'vue'
 import { firebaseApp } from '@/plugins/firebase';
-import { getAuth, signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
 const auth = getAuth(firebaseApp);
 const provider = new GoogleAuthProvider();
-
+let button_disable = ref(false);
 let msg = ref("login")
+let userdata = ref({
+    name: null,
+    email: null,
+    token: null
+});
 
 const test = () => { console.log(msg.value); msg.value = "t" }
 const toggleSignIn = () => {
@@ -14,9 +19,13 @@ const toggleSignIn = () => {
 
             msg = "SignOut";
             const credential = GoogleAuthProvider.credentialFromResult(res);
-            //const token = credential.accessToken;
+            const token = credential.accessToken;
             const user = res.user;
-
+            userdata.value = {
+                name: user.displayName,
+                email: user.email,
+                token: token
+            }
             console.log(user);
         }).catch(function (error) {
             // Handle Errors here.
@@ -35,13 +44,30 @@ const toggleSignIn = () => {
     } else {
         signOut(auth);
     }
+    button_disable.value = true;
+
 }
+onAuthStateChanged(auth, user => {
+    if (user) {
+        console.log(user);
+        userdata.value = {
+            name: user.displayName,
+            email: user.email,
+            token: user.accessToken
+        }
+        msg.value = "logout";
+    } else {
+        msg.value = "login";
+    }
+    button_disable.value = false;
+});
 </script>
 <template>
     <div class="signin">
         <h2>Sign in</h2>
-        <button @click="toggleSignIn">{{ msg }}</button>
+        <button :disabled="button_disable" @click="toggleSignIn">{{ msg }}</button>
         <button @click="test">test</button>
+        <p>user:{{ userdata.name }}<br>email:{{ userdata.email }}<br></p>
     </div>
 </template>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
